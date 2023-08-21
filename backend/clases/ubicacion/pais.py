6c9ee5628132578c4a1fa7bba1adca89317d0226
josmardiaz92 <josmardiaz92@gmail.com>
2023-08-21 19:58:36 -0400

    class pais lista

--- backend/clases/ubicacion/pais.py
+++ backend/clases/ubicacion/pais.py
@@ -2,12 +2,6 @@ import sys
 sys.path.append('c:\\xampp\\htdocs\\Github\\redsocial-fanpage')
 
 from backend.clases.base_datos import postgres_conexion as bd
-
-
-""" #TODO clase para la tabla pais
-#*Importación de la clase de base de datos
-from ..base_datos import postgres_conexion as bd #Tambien se puede: import base_datos.postgres_conexion as bd """
-
 class pais(bd.base_datos):
     #*Funcion constructora, los parametros seran el nombre de las columnas en la base de datos
     def __init__(self):
@@ -27,7 +21,7 @@ class pais(bd.base_datos):
         return contador #*retornamos el contador
     def modificar(self):
         #*Llamamos a la funcion para modificar en la base de datos
-        sql=f"""select ubicacion.pais_modificarar({self.cod_pai},'{self.nom_pai}','{self.des_pai}','{self.ali_pai}','{self.cti_pai}',{self.fky_pai},'{self.est_pai}')"""
+        sql=f"""select ubicacion.pais_modificar({self.cod_pai},'{self.nom_pai}','{self.des_pai}','{self.ali_pai}','{self.cti_pai}',{self.fky_pai},'{self.est_pai}')"""
         self.cursor.execute(sql) #*Le decimos al cursor que deseamos ejecutar la sentencia sql
         self.conexion.commit() #*Confirmamos la ejecución de la sentencia sql
         contador=self.cursor.rowcount #*contador de resultados
@@ -46,16 +40,3 @@ class pais(bd.base_datos):
         self.conexion.commit() #*Confirmamos la ejecución de la sentencia sql
         contador=self.cursor.rowcount #*contador de resultados
         return contador #*retornamos la contador
-    
-
-#*Instanciamos un objeto
-p=pais()
-p.conectar()
-p.nom_pai='III Reich'
-p.des_pai='Pais de pedroschausen'
-p.ali_pai='Pedronshafnchausen'
-p.cti_pai='+74'
-p.fky_pai=1
-p.est_pai='A'
-p.agregar()
-p.cerrar()
